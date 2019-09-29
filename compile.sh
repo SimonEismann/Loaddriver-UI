a9c46d481d5d08771ebe9c7036d765283f12dbb4
Long Bui <longbui68@yahoo.de>
2019-09-29 21:34:25 +0200

    Update example setup to include frontend

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
 docker build -t simoneismann/loaddriver-master ./loaddriver-master
 docker build -t simoneismann/loaddriver-slave ./loaddriver-slave
+docker build -t simoneismann/loaddriver-frontend ./loaddriver-frontend
