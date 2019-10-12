9cc18fbe3ccabaa1555a1541e4302e5e3234de44
Long Bui <longbui68@yahoo.de>
2019-10-12 12:23:02 +0200

    Update to build latest tag

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
-docker build -t simoneismann/loaddriver-master ./loaddriver-master
-docker build -t simoneismann/loaddriver-slave ./loaddriver-slave
-docker build -t simoneismann/loaddriver-frontend ./loaddriver-frontend
+docker build -t simoneismann/loaddriver-master:latest ./loaddriver-master
+docker build -t simoneismann/loaddriver-slave:latest ./loaddriver-slave
+docker build -t simoneismann/loaddriver-frontend:latest ./loaddriver-frontend
