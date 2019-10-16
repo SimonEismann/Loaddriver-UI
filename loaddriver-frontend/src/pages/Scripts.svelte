305d4a1c2451b380e59e9e25a79533152f5e04f0
Long Bui <long.bui@stud-mail.uni-wuerzburg.de>
2019-10-16 18:35:39 +0200

    Add forgotten files

---
+++ loaddriver-frontend/src/pages/Scripts.svelte
@@ -0,0 +1,13 @@
+<style>
+  .container {
+    width: 100%;
+    height: 100%;
+    padding-top: 2em;
+    padding-left: 30%;
+  }
+</style>
+
+<div class="container">
+  <h1>Under construction</h1>
+  <p>This page is currently under construction.</p>
+</div>
